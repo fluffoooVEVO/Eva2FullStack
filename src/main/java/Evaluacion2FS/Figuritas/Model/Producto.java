3c49e17326816f82efa9e9209746f04f7e266ca3
Javier Rodriguez <javie.rodriguezc@duocuc.cl>
2026-05-04 16:12:22 -0400

    chore:init

---
+++ src/main/java/Evaluacion2FS/Figuritas/Model/Producto.java
@@ -0,0 +1,28 @@
+package Evaluacion2FS.Figuritas.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="Producto")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message="El producto no puede quedar con atributo vacio")
+    @Column(nullable=false,length=60)
+    private String nombre;
+
+}
