dfbd30b7b43acfce83a70cdd210e38a071b3413c
Daniel <dancode99@gmail.com>
2026-05-07 00:35:22 -0400

    fix: cambio para forzar git 2

--- src/main/java/Evaluacion2FS/Figuritas/Model/Enlaces.java
+++ src/main/java/Evaluacion2FS/Figuritas/Model/Enlaces.java
@@ -8,12 +8,4 @@ public class Enlaces {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_enlace;
-
-    // el nombre del enlace es obligatorio y maximo 100 caracteres
-    @Column(nullable = false, length = 100)
-    private String nombre;
-
-    // la url tambien es obligatoria para que no guarden enlaces vacios
-    @Column(nullable = false, length = 255)
-    private String url;
 }
