ffb3396556bf783616aca1c7b9856030e91003d2
Javier Rodriguez <javie.rodriguezc@duocuc.cl>
2026-05-08 11:26:55 -0400

    Avances logs, DTO y conexiones

--- src/main/java/Evaluacion2FS/Figuritas/FiguritasApplication.java
+++ src/main/java/Evaluacion2FS/Figuritas/FiguritasApplication.java
@@ -9,5 +9,4 @@ public class FiguritasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FiguritasApplication.class, args);
 	}
-
 }
---
+++ src/main/java/Evaluacion2FS/Figuritas/Model/Enlaces.java
@@ -0,0 +1,9 @@
+package Evaluacion2FS.Figuritas.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+public class Enlaces {
+    //prueba de commit
+}
